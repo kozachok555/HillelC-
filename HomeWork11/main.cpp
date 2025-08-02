723feb09be1fa1cdd9d2e7e9d56441b340091086
kozachok555 <kz4nkovova@gmail.com>
2025-08-02 21:05:30 +0200

    Done HW11

--- HomeWork11/main.cpp
+++ HomeWork11/main.cpp
@@ -24,9 +24,16 @@ int calculate(char op, int firstNumber, int secondNumber) {
     return 0;
 }
 int calculate(const char* op, const int* firstNumber, const int* secondNumber) {
-    if (op && firstNumber && secondNumber)
-        return calculate(*op, *firstNumber *secondNumber);
+    if (op && firstNumber && secondNumber) 
+        return calculate(*op, *firstNumber, * secondNumber);
+
 }
+int& processFirstBy(const char op, int& firstNumber, const int secondNumber)
+{
+    firstNumber = calculate(op, firstNumber, secondNumber);
+    return firstNumber;
+}
+
 int main() {
     int* firstNumber = new int;
     int* secondNumber = new int;
@@ -47,5 +54,16 @@ int main() {
     op = nullptr;
     std::cout << "Result: " << result << std::endl;
 
+    int  first1 = 0;
+    int second1 = 0;
+    char op1 = 0;
+    std::cout << "Enter first number: ";
+    std::cin >> first1;
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op1;
+    std::cout << "Enter second number: ";
+    std::cin >> second1;
+    int res = processFirstBy(op1, first1, second1);
+	std::cout << "Result: " << res << std::endl;
     return 0;
 }
