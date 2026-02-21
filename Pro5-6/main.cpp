618cf7a82d0694805066ef7597d0d22b5957422a
kozachok555 <kz4nkovova@gmail.com>
2026-02-21 19:37:27 +0200

    done hw pro5-6

---
+++ Pro5-6/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "math/math.hpp" 
+
+int main() {
+    std::cout << add(2, 3) << "\n";
+    return 0;
+}
---
+++ Pro5-6/math/math.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+extern "C" {
+	int add(int a, int b);
+	int sub(int a, int b);
+	int mul(int a, int b);
+	int mydiv(int a, int b);
+	int myfactorial(int n);
+	int mylog(int n);
+	int mypow(int base, int exp);
+	int mysqrt(int n);
+}
